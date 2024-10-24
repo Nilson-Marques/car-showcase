f4f38aabed53de97682fb9fdf428fa08302e2899
codewalke <nilsonmarquesdonascimento@gmail.com>
2024-10-24 09:08:34 -0300

    api3

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,7 +4,7 @@ import { CustomButtomProps } from '@/types';
 import Image from 'next/image';
 
 const CustomButton = ({ title, containerStyles,
-   handleClick, btnType }: CustomButtomProps) => {
+   handleClick, btnType, textStyles, rightIcon }: CustomButtomProps) => {
   return (
     <button 
     disabled={false}
@@ -13,9 +13,19 @@ const CustomButton = ({ title, containerStyles,
       `}
     onClick={handleClick}
     >
-    <span className={'flex-1'}>
+    <span className={`flex-1 ${textStyles}`}>
         {title}
     </span>
+    {rightIcon && (
+    <div className="relative w-6 h-6">
+      <Image
+        src={rightIcon}
+        alt="right icon"
+        fill
+        className="object-contain"
+      />
+    </div>
+    )}
     </button>
   )
 }
