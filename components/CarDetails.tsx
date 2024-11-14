ce1094c9d5f9bd8b3271cb635e1287bf6688df54
codewalke <nilsonmarquesdonascimento@gmail.com>
2024-11-13 22:52:46 -0300

    api.image

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -44,7 +44,7 @@ const CarDetails = ({ isOpen, closeModal, car }:
             leaveFrom="opacity-100 scale-100"
             leaveTo="opacity-0 scale-95"
         >
-          <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto  transform rounded-2xl bg-white p-6 text-left shadow-xl transsition-all flex flex-col">
+          <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto  transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
             <button 
               type="button"
               className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
